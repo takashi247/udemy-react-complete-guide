529369f06754586668a1aa4bcb809d35113615aa
takashi247 <takashi247@gmail.com>
2022-10-05 23:56:07 +0900

    add examples until 4-10

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import Expenses from "./components/Expenses";
+import React from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -25,7 +28,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
